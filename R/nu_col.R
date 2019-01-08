0147ccc56901cf9c8adabd0eae9402eb603101e3
Matt Peyton <mattpeyton1@gmail.com>
2019-01-08 16:17:08 -0600

    Updated naming and added bright and dark color themes

---
+++ R/nu_col.R
@@ -0,0 +1,49 @@
+#' NU Color Function
+#'
+#' This function pulls in Northwestern colors
+#' @keywords NU
+#' @export
+#' @examples
+#' nu_cols
+
+nu_col <- c(
+  purple      = "#4E2A84",
+  purple_lt   = "#B6ACD1",
+  purple_dk   = "#401F68",
+  green       = "#58B947",
+  teal        = "#7FCECD",
+  blue        = "#5091CD",
+  yellow      = "#EDE93B",
+  orange      = "#FFC520",
+  red         = "#EF553F",
+  green2      = "#008656",
+  teal2       = "#007FA4",
+  blue2       = "#0D2D6C",
+  yellow2     = "#D9C826",
+  orange2     = "#CA7C1B",
+  red2        = "#D85820"
+)
+
+nu_bright <- c(
+  green       = "#58B947",
+  teal        = "#7FCECD",
+  blue        = "#5091CD",
+  yellow      = "#EDE93B",
+  orange      = "#FFC520",
+  red         = "#EF553F",
+  purple      = "#4E2A84",
+  purple_lt   = "#B6ACD1",
+  purple_dk   = "#401F68"
+)
+
+nu_dark <- c(
+  green      = "#008656",
+  teal       = "#007FA4",
+  blue       = "#0D2D6C",
+  yellow     = "#D9C826",
+  orange     = "#CA7C1B",
+  red        = "#D85820",
+  purple      = "#4E2A84",
+  purple_lt   = "#B6ACD1",
+  purple_dk   = "#401F68"
+)
