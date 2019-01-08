35bd6ef404589b7f27b8e517da69800a1eb0bcc5
Matt Peyton <mattpeyton1@gmail.com>
2019-01-08 12:16:54 -0600

    Fixed color formatting

--- R/nu_cols.R
+++ R/nu_cols.R
@@ -8,10 +8,7 @@
 
 # Structure taken from https://github.com/kbroman/pkg_primer/blob/gh-pages/example/stage1/R/brocolors.R
 
-nu_cols <-
-  function(set=c("general"))
-{
-  general <- c(
+nu_cols <- c(
   purple      = "#4E2A84",
   purple_lt   = "#B6ACD1",
   purple_dk   = "#401F68",
@@ -29,6 +26,18 @@ nu_cols <-
   red2        = "#D85820"
   )
 
-  switch(match.arg(set),
-       nu_cols=nu_cols)
-}
+purple      = "#4E2A84"
+purple_lt   = "#B6ACD1"
+purple_dk   = "#401F68"
+green       = "#58B947"
+teal        = "#7FCECD"
+blue        = "#5091CD"
+yellow      = "#EDE93B"
+orange      = "#FFC520"
+red         = "#EF553F"
+green2      = "#008656"
+teal2       = "#007FA4"
+blue2       = "#0D2D6C"
+yellow2     = "#D9C826"
+orange2     = "#CA7C1B"
+red2        = "#D85820"
